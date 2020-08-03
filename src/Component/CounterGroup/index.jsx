c374bf3ed11042816d9d284a5013ae4eaa52962c
986224568 <986224568@qq.com>
2020-08-03 23:03:28 +0800

    [countReact][Domingo] bug-fix rename total

--- src/Component/CounterGroup/index.jsx
+++ src/Component/CounterGroup/index.jsx
@@ -7,13 +7,12 @@ class CounterGroup extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state ={total:store.getState(),size:0}
+        this.state ={value:store.getState(),size:0}
     }
 
     handleResize = (event) => {
         this.setState({
             size: !event.target.value ? 0 : parseInt(event.target.value)>=0 ?  parseInt(event.target.value): 0,
-            total: 0
         })
         store.dispatch(init());
     }
@@ -21,7 +20,7 @@ class CounterGroup extends React.Component {
     render() {
         store.subscribe(() =>{
             this.setState({
-                total:store.getState()
+                value:store.getState()
             })
         })
         const initArray = [...Array(this.state.size).keys()];
@@ -33,7 +32,7 @@ class CounterGroup extends React.Component {
                     <input onBlur={this.handleResize} defaultValue={0}></input>
                 </label><br></br>
                 <label>
-                    Total Number: {this.state.total}
+                    Total Number: {this.state.value}
                 </label>
                     {
                         initArray.map(key =><Counter count={this.state.size}   key={key}/>)
